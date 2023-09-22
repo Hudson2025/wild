2eac20cedce8c97a079a8cf17edd3e4d3ec03f7f
Hudson Land <hudson.land@mrjaffesclass.com>
2023-09-22 11:35:09 -0700

    wild

---
+++ Wildloops.java
@@ -0,0 +1,18 @@
+public class Wildloops
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        int i = 0;
+        
+        
+    }
+    
+    
+    
+    }
+
