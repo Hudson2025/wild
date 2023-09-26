98e13051b360f9e84aab84ada108a21af61dde1e
Hudson Land <hudson.land@mrjaffesclass.com>
2023-09-26 11:05:25 -0700

    do

--- DoWhile.java
+++ DoWhile.java
@@ -1,4 +1,4 @@
-public class Wildloops
+public class DoWhile
 {
 
     int a = 5;
@@ -8,24 +8,23 @@ public class Wildloops
 
     public void run() {
         int i = 0;
-        while (i < 15) {
+        do {
             System.out.println("i is" + i + "but is less than 15");
             i = i + 1;
-        }
-
+        }while (i < 15);
         int k = 7;
-        while (k > 5) {
+        do {
             System.out.println("k is" + k + "but is greater than 5");
             k = k -1;
-        }
+        }while (k > 5);
 
         int a = 8;
         int b = 16;
-        while (a < 10 && b <= 20) {
+        do {
             System.out.println("a is" + a + "but is less than 10");
             System.out.println("b is" + b + "but is less than or equal to 20");
             a = a + 1;
             b = b + 1;
-        }
+        }while (a < 10 && b <= 20);
     }
 }
